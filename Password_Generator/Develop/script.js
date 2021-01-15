27a6091b07ccf7f74d6ced04b66c4fb1a6823b29
Hancong Wang <wang.hanc@northeastern.edu>
2021-01-15 02:41:01 -0800

    completed the password generator

---
+++ Password_Generator/Develop/script.js
@@ -0,0 +1,82 @@
+// Assignment Code
+var contBtn = document.querySelector("#Continue");
+var modal = document.getElementById("MyModal");
+// validate the length input
+function validateLength(len) {
+  var n = Number(len);
+  if (Number.isInteger(n) && n >= 8 && n <= 128) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// generate the password meeting the criteriaa
+function generatePassword() {
+  var lengthInput = document.getElementById("lenInput").value;
+  var ifLowercase = document.getElementById("Lowercase").checked;
+  var ifUppercase = document.getElementById("Uppercase").checked;
+  var ifNumeric = document.getElementById("Numeric").checked;
+  var ifSpecial = document.getElementById("Special").checked;
+  // Both length input requirement and character type requirement are met
+  if (validateLength(lengthInput) && (ifLowercase || ifUppercase || ifNumeric || ifSpecial)) {
+    modal.style.display = "none";
+  } else { // otherwise
+    while (!validateLength(lengthInput)) { // while length input is invalid, repeatedly prompt for the valid length input
+        lengthInput = prompt("Please enter a valid length (note: the length should be at least 8 and no more than 128)!");
+      }
+      document.getElementById("lenInput").value = lengthInput;
+      // if no character type is chosen
+      while (!ifLowercase && !ifUppercase && !ifNumeric && !ifSpecial) {
+          alert("Please choose at least on character type!");
+          ifLowercase = confirm("Would you like to include lowercase letters?");
+          ifUppercase = confirm("Would you like to include uppercase letters?");
+          ifNumeric = confirm("Would you like to include numeric characters?");
+          ifSpecial = confirm("Would you like to include special characters?");
+      }
+      document.getElementById("Lowercase").checked = ifLowercase;
+      document.getElementById("Uppercase").checked = ifUppercase;
+      document.getElementById("Numeric").checked = ifNumeric;
+      document.getElementById("Special").checked = ifSpecial;
+      document.getElementById("Continue").onclick = function() {
+        modal.style.display = "none";
+      }
+  }
+  const alphabetArray = "abcdefghijklmnopqrstuvwxyz".split("");
+  const digitArray = "0123456789".split("");
+  const specialCharArray = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
+  var password = "";
+  var index = 0;
+  // build the password
+  while (index < lengthInput) {
+    if (ifLowercase) {
+      var randAlphabetIndex = Math.floor((Math.random() * alphabetArray.length) + 1) - 1;
+      password += alphabetArray[randAlphabetIndex];
+      index++;
+    }
+    if (ifUppercase && index < lengthInput) {
+      var randIndex = Math.floor((Math.random() * alphabetArray.length) + 1) - 1;
+      password += alphabetArray[randIndex].toUpperCase();
+      index++;
+    }
+    if (ifNumeric && index < lengthInput) {
+      var randDigitIndex = Math.floor((Math.random() * digitArray.length) + 1) - 1;
+      password += digitArray[randDigitIndex];
+      index++;
+    }
+    if (ifSpecial && index < lengthInput) {
+      var randSpecialIndex = Math.floor((Math.random() * specialCharArray.length) + 1) - 1;
+      password += specialCharArray[randSpecialIndex];
+      index++;
+    }
+  }
+  return password;
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+contBtn.addEventListener("click", writePassword);
---
+++ Password_Generator/README.md
@@ -0,0 +1,12 @@
+## Password Generator
+#### Description
+Create an application that users can use to generate a random password based on criteria they’ve selected. The application has a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
+#### Deployed Application
+![Portfolio Page](Assets/Images/page.png)
+Here is the [link to the deployed website](https://herita7171.github.io/Herita7171/Responsive_Portfolio/Develop/page/).
+#### Review
+* Generate random passwords that matches the selected criteria
+* Produce no errors in the console
+* Intuitive and easy-to-navigate user experience
+* Clean and polished user interface style
+* Deployed application
